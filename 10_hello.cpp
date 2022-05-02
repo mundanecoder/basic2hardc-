a5b1f1319644d9a6423ccc276bb3fda6d7f64f43
mundanecoder <99796187+mundanecoder@users.noreply.github.com>
2022-05-02 22:55:09 +0530

    Create 10_hello.cpp

---
+++ 10_hello.cpp
@@ -0,0 +1,21 @@
+//print hello in for user entered 
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+
+    cin>>a;
+
+    for(int i=0;i<a;i++){
+     if (a>i)
+    {
+        cout<<"hello world"<<endl;
+    }
+}
+    
+        
+}
