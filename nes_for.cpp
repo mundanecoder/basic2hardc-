7778d9df4326abd49c32b06ffbad46ec56c0bee1
mundanecoder <99796187+mundanecoder@users.noreply.github.com>
2022-05-02 23:03:40 +0530

    Create nes_for.cpp

---
+++ nes_for.cpp
@@ -0,0 +1,28 @@
+//nested for loop
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int a,b;
+cin>>a>>b;
+
+int i,num;
+
+for(num=a;num<=b;num++)
+{
+    for( i=2;i<=num;i++){
+        if(num%i==0){
+           
+            break;        }
+    }
+    if(i==num){
+        cout<<num<<endl;
+
+    }
+   
+}
+}
