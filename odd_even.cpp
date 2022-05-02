1c302d057cb9a9ba8a35e698035cb79d9800c822
mundanecoder <99796187+mundanecoder@users.noreply.github.com>
2022-05-02 22:58:41 +0530

    Create odd_even.cpp

---
+++ odd_even.cpp
@@ -0,0 +1,23 @@
+#to determine whether nos are odd or even
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+    int a;
+
+    cin>>a;
+
+    if(a%2==0){
+        cout<<"no is even"<<endl;
+
+    }
+    else{
+        cout<<"no is odd"<<endl;
+    }
+}
+
